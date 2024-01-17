fcc5fec112aae9c7d8801235bd94b936e140e840
Pedro Souza <pedrocsouza97@gmail.com>
2024-01-17 13:35:06 -0300

    Create celciusParaFahrenheit.java

---
+++ exerc-logica/src/celciusParaFahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/*CRIE UM PROGRAMA QUE RECEBA UMA TEMPERATURA EM
+* GRAUS ºC E RETORNE O VALOR EQUIVALENTE EM ºF*/
+public class celciusParaFahrenheit {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        
+
+
+
+
+
+
+        read.close();
+    }
+}
