4dcbcf2dc941922a0fba8a6335bd66f8fef9a6e8
psicoder0101 <pedrocsouza97@gmail.com>
2024-02-28 13:26:59 -0300

    created TesteFuncoes exercise class not done yet

---
+++ exerc-repeticao/src/TesteFuncoes.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/* criar funcoes de checagem de input que consigam validar o tipo de entrada, garantir
+* a segurança do codigo e que repitam o processo de leitura caso o tipo de entrada seja
+* incompativel */
+
+
+public class TesteFuncoes {
+   static Scanner read = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.print("Digite um numero: ");
+        int num = inputCheckSec();
+
+
+    }
+
+//    funcao simples que apenas le e repete em caso de erro
+    public static int inputcheck () {
+        int num = 0;
+        boolean check = false;
+        while (!check) {
+            if (read.hasNextInt()) {
+                /*a entrada apenas com hasNext pode permitir injecao de codigo
+                * ou buffer overflow*/
+                num = read.nextInt();
+                check = true;
+            } else {
+                System.out.println("FORMATO INVALIDO! DIGITE NOVAMENTE: ");
+            }
+        }
+        return num;
+    }
+
+    /* funcao com leitura e correcao, implementando seguranca conta injecao de codigo
+     * e buffer overflow */
+    public static int inputCheckSec () {
+        int num = 0;
+        boolean check = false;
+        while (!check) {
+            String input = read.nextLine();
+            /*implementa a leitura a partir de uma string qualquer*/
+            if (input.matches("[0-9]+")) {
+                /*usa regex para buscar padroes numericos e garante que apenas
+                * numeros serao aceitos na entrada*/
+                num = Integer.parseInt(input);
+                /*caso seja digitado um numero na string, converte para int*/
+                check = true;
+            } else {
+                System.out.println("FORMATO INVALIDO! DIGITE NOVAMENTE: ");
+            }
+        }
+        return num;
+    }
+
+
+/* ok ate aqui, falta reescrever a funcao com correcao para erro DoS, tambem tentar escrever
+* funcoes para outros tipos de variaveis na entrada, tentar fazer com Double, por exemplo
+* que nao eh aceito na funcao acima porque, apesar de ser numero, tem virgula no meio, verificar
+* se é por isso mesmo ou se nao passa porque o ParseInt so converte inteiros mesmo*/
+}
