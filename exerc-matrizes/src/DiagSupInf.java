bd5c4808eedc746766ad162289cbfd5559cc24e5
Pedro Souza <pedrocsouza97@gmail.com>
2024-07-02 22:49:05 -0300

    DiagSupInf exercise class finished

--- exerc-matrizes/src/DiagSupInf.java
+++ exerc-matrizes/src/DiagSupInf.java
@@ -1,9 +1,10 @@
 /*crie um programa que imprima o triangulo superior ou o inferior de uma matriz
 * a depender da escolha do usuario*/
 
+import java.util.Scanner;
 public class DiagSupInf {
-
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
 
         System.out.println("=== IMPRESSAO DE DIAGONAL ===");
         System.out.println("MATRIZ COMPLETA:");
@@ -25,23 +26,52 @@ public class DiagSupInf {
             System.out.println();
         }
 
+        //selecao da diagonal a ser impressa
+        System.out.print("Digite qual triângulo da matriz você deseja imprimir" +
+                "\n[1] Superior" +
+                "\n[2] Inferior" +
+                "\nR> ");
+        int resposta = read.nextInt();
+
+        //processamento da saida
+        switch (resposta){
+            case 1 -> imprimirDiagSup(palavras);
+            case 2 -> imprimirDiagInf(palavras);
+            default -> System.out.println("ERRO: RESPOSTA INVALIDA!");
+        };
+
+        read.close();
+    }
+
+    public static void imprimirDiagSup (char[][] matriz) {
+        System.out.println("_________________________________________________");
+        System.out.println("\nDIAGONAL SUPERIOR: ");
+
+        //imprime apenas os elementos acima da diagonal principal, incluindo a propria diagonal
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length - i; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void imprimirDiagInf(char[][] matriz) {
+        System.out.println("_________________________________________________");
         System.out.println("\nDIAGONAL INFERIOR: ");
 
         //imprime apenas os elementos abaixo da diagonal principal, incluindo a propria diagonal
-        for (int i = 0; i < palavras.length; i++) {
-            for (int j = 0; j < palavras[i].length; j++) {
-                if (j < palavras[i].length - (i + 1)) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (j < matriz[i].length - (i + 1)) {
                     System.out.print("  ");
                 } else {
-                    System.out.print(palavras[i][j] + " ");
+                    System.out.print(matriz[i][j] + " ");
                 }
             }
             System.out.println();
         }
-
     }
-}
 
-/*PARA CONTINUAR DEPOIS: Preciso transformar esse trecho de impressao da diagonal
-* inferior em uma funcao. criar tambem uma funcao com o codigo do ultimo exercicio
-* que imprime a diagonal superior*/
+    //codigo funcionando 02 de julho de 2024
+}
